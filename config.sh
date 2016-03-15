d2b056722ecbb1b535a3922c1b35fcc1ea099de0
John Angel <john.angel@tdstelecom.com>
2016-03-15 14:42:33 -0500

    Added name

--- config.sh
+++ config.sh
@@ -3,12 +3,13 @@
 set -e
 set -x
 
-SERVER_IP_ADDR=192.168.19.15
+sysctl kernel.name="client"
+SERVER_IP_ADDR=192.168.56.101
 SERVER_FQDN=ipaserver.example.com
 SERVER_NAME=ipaserver
 IPA_REALM=EXAMPLE.COM
 IPA_DOMAIN=example.com
-CLIENT_IP_ADDR=192.168.19.20
+CLIENT_IP_ADDR=192.168.56.20
 CLIENT_FQDN=`hostname`
 CLIENT_NAME=`hostname | cut -d. -f 1 | tr '[:upper:]' '[:lower:]'`
 PASSWORD=aaaAAA111
