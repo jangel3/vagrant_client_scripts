b91ef6cb6025d3b219b0cd8744a8da6eb9b30515
John Angel <john.angel@tdstelecom.com>
2016-03-23 14:09:31 -0500

    Needed to make public zone active.

--- install.sh
+++ install.sh
@@ -47,6 +47,8 @@ firewall-cmd --zone=public --add-port 464/udp
 firewall-cmd --zone=public --add-port  53/udp
 firewall-cmd --zone=public --add-port 123/udp
 
+firewall-cmd --set-default-zone=public
+
 echo "Installing IPA client ..."
 ipa-client-install --enable-dns-updates --ssh-trust-dns -p admin -w $PASSWORD -U
  
